f3b2eba66bf1e5fe8650aba2cb08c90e6aaca748
Gayatri Kancherla Ramesh <krgayatri.30@gmail.com>
2026-03-04 14:48:42 -0800

    Add files via upload

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        Welcome to Luxora, your ultimate destination for a seamless online shopping experience. We aim to redefine e-commerce by offering a diverse range of high-quality products with an intuitive and engaging platform. Whether you're looking for fashion, electronics, or everyday essentials, Luxora provides a smooth and secure shopping experience, ensuring convenience at every step. Our goal is to make online shopping effortless, fast, and enjoyable for everyone.
+
+         Built with modern technologies, Luxora leverages React and Redux for a dynamic user interface, Bootstrap for a responsive and elegant design, and the Fake Store API to deliver real-time product data. With a focus on performance, security, and user experience, Luxora is designed to bring you a hassle-free and efficient way to shop. Explore our collections and enjoy a smarter way to shop online!
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
